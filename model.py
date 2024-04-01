34c8588edb2a1cad4778a82ab20e0008c5652a4b
ShadowD-9 <158149960+ShadowD-9@users.noreply.github.com>
2024-04-01 09:00:28 -0600

    Update model.py

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ from langchain.vectorstores import FAISS
 from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
 import tempfile
+from langchain.document_loaders.csv_loader import CSVLoader
 
 DB_FAISS_PATH = 'vectorstores/db_faiss'
 
